b7ed99c75da39b8afb34fe4f2a590b38e69c3c67
yourUsername <yourEmail@domain.com>
2020-01-26 16:56:29 -0500

    dd

---
+++ something/src/main/java/com/example/demo/geffry.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class geffry {
+public static void main(String[]args) {
+
+    Scanner input = new Scanner(System.in);
+
+// using array and add a number start from index and going up to 10 which means 9.
+
+    int[]list = new int[10];
+
+
+list[0]=100;
+list[1]=200;
+list[2]=300;
+list[3]=400;
+list[4]=500;
+list[5]=600;
+list[6]=700;
+list[7]=800;
+list[8]=900;
+list[9]=1000;
+
+System.out.println("Element at index 0:"+ list[0]);
+    System.out.println("Element at index 1:"+ list[1]);
+    System.out.println("Element at index 2:"+ list[2]);
+    System.out.println("Element at index 3:"+ list[3]);
+    System.out.println("Element at index 4:"+ list[4]);
+    System.out.println("Element at index 5:"+ list[5]);
+    System.out.println("Element at index 6:"+ list[6]);
+    System.out.println("Element at index 7:"+ list[7]);
+    System.out.println("Element at index 8:"+ list[8]);
+    System.out.println("Element at index 9:"+ list[9]);
+
+
+
+
+
+
+}
+
+}
